e534668a530aa4f48be916936963f7170d67f629
Chihaya Taira <prelude371@gmail.com>
2019-05-29 16:02:29 +0900

    add GroupList component

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,14 @@
 class GroupsController < ApplicationController
   def index
+    per = params[:per].to_i.clamp(10, 60)
     @groups = Group.all
+    if params[:search]
+      p = params[:search]
+      if p[:name].present?
+        @groups = Group.search(p[:name])
+      end
+    end
+    @groups = @groups.page(params[:page]).per(per)
+    render json: {groups: @groups, users: @groups.map{|group| [group.id, group.users]}.to_h, total_count: @groups.total_count}
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -7,4 +7,15 @@ class Group < ApplicationRecord
 
   validates :code, presence: true
   validates :name, presence: true
+
+  def self.search(term)
+    term = term.gsub(/\p{Blank}/, '')
+    groups = Group.all
+    if term =~ /^[0-9a-zA-Z]+$/
+      groups = groups.where('code LIKE ?', '%' + term + '%')
+    else
+      groups = groups.where('name LIKE ?', '%' + term + '%')
+    end
+    groups
+  end
 end
